27003e0892f3b03ef4c0389845c09a5e68f19a44
nintyning <nintyning@gmail.com>
2018-08-24 23:04:33 -0500

    new member window code updates

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import *
 from ui_newmember import *
-from newmember import *
+from sign_in import *
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mywin = NewMember()
+    mywin = Singin()
     mywin.show()
     app.exec_()
