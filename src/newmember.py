ab187f1256cd93c01fdbf10bf8f2565fc78af620
nintyning <nintyning@gmail.com>
2018-08-24 20:31:00 -0500

    fix error

---
+++ src/newmember.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+from ui_newmember import *
+from PyQt5.QtCore import pyqtSlot
+
+class NewMember(QMainWindow, Ui_NewMember):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn_cancel.released.connect(self.close)
+        self.tb_kor_name.setReadOnly(False)
+    @pyqtSlot()
+    def close(self):
+        exit()
