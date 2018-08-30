4210cc8009ff685883925d8487e6b88f1b0625dd
Bora Yang <bora.yang@mavs.uta.edu>
2018-08-29 21:38:44 -0500

    add home

---
+++ src/view_dept.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+from ui_view_dept import *
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtCore import QDate
+from db_connect_singleton import *
+from PyQt5.Qt import QImage, QFile, QFileDialog, QPixmap
+
+class Home(QMainWindow, Ui_Home):
+    myWindowCloseSignal = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn_quit.released.connect(self.closeClicked)
